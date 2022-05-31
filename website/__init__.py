6055ea3f08535f7e2ea948e9b07950950372b5c0
hmu31 <huimin_mu1@gatech.edu>
2022-05-31 12:51:46 -0500

    Initialize the Database File

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,6 @@
 # import sys
 # print(sys.path)
+import os
 import imp
 from flask import Flask
 
@@ -7,16 +8,27 @@ from flask import Flask
 # whenever we import website folder, code in this file will run automatically
 def create_app():
     # initialize the app
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
     # secure the cookies and session data related to our website, random strings, in production, dont share
-    app.config['SECRET_KEY'] = 'aljsfkjdlkjflsf'
-    
+    app.config.from_mapping(
+        SECRET_KEY = 'dev',
+        DATABASE = os.path.join(app.instance_path, 'website.sqlite')
+    )
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .import db
+    db.init_app(app)
     
     return app
 
